ddf31688fe7dc8cf22eee52e43dbfc3b1c2e1c2f
Chinmay-903 <144786163+Chinmay-903@users.noreply.github.com>
2024-11-18 22:56:50 +0530

    Update app.py

--- BRAIN TUMOR IMAGE CLASSIFICATION ML Project/app.py
+++ BRAIN TUMOR IMAGE CLASSIFICATION ML Project/app.py
@@ -11,6 +11,9 @@ from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
+port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
+app.run(host="0.0.0.0", port=port)
+
 
 model =load_model('BrainTumor10EpochsCategorical.keras')
 print('Model loaded. Check http://127.0.0.1:5000/')
